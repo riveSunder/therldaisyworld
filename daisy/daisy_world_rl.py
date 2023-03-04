8fdf38ee86e6154e0ca4e04ec903d39bac3803a3
rivesunder <rivesunder@protonmail.com>
2023-03-04 14:11:29 -0700

    add save/load/restore/apply functions for rl daisyworld configs (dict->json)

--- daisy/daisy_world_rl.py
+++ daisy/daisy_world_rl.py
@@ -1,3 +1,7 @@
+import os
+
+import json
+
 import numpy as np
 import time
 
@@ -53,6 +57,86 @@ class RLDaisyWorld():
         self.initialize_neighborhood()
         self.initialize_agents()
         self.reset()
+
+
+    def make_config(self):
+
+        config = {}
+
+        config["max_L"] = self.max_L
+        config["min_L"] = self.min_L
+        config["initial_L"] = self.initial_L
+        config["ramp_period"] = self.ramp_period
+        config["dL"] = self.dL
+        config["p"] = self.p
+        config["g"] = self.g
+        config["S"] = self.S
+        config["sigma"] = self.sigma
+        config["gamma"] = self.gamma
+        config["albedo_bare"] = self.albedo_bare
+        config["albedo_light"] = self.albedo_light
+        config["albedo_dark"] = self.albedo_dark
+        config["temp_optimal"] = self.temp_optimal
+        config["light_proportion"] = self.light_proportion
+        config["dark_proportion"] = self.dark_proportion
+        config["initial_al"] = self.initial_al
+        config["initial_ad"] = self.initial_ad
+        config["n_agents"] = self.n_agents
+        config["agent_gamma"] = self.agent_gamma
+
+        return config
+
+    def save_config(self, filepath=None):
+
+        if filepath is None:
+            filepath = os.path.join("results", "default_model_config.json")
+
+        config = self.make_config()
+
+        with open(filepath, "w") as f:
+            json.dump(config, f)
+
+    def _apply_config(self, config):
+
+        self.max_L = config["max_L"]
+        self.min_L = config["min_L"]
+        self.initial_L = config["initial_L"]
+        self.ramp_period = config["ramp_period"]
+        self.dL = config["dL"]
+        self.p = config["p"]
+        self.g = config["g"]
+        self.S = config["S"]
+        self.sigma = config["sigma"]
+        self.gamma = config["gamma"]
+        self.albedo_bare = config["albedo_bare"]
+        self.albedo_light = config["albedo_light"]
+        self.albedo_dark = config["albedo_dark"]
+        self.temp_optimal = config["temp_optimal"]
+        self.light_proportion = config["light_proportion"]
+        self.dark_proportion = config["dark_proportion"]
+        self.initial_al = config["initial_al"]
+        self.initial_ad = config["initial_ad"]
+        self.n_agents = config["n_agents"]
+        self.agent_gamma = config["agent_gamma"]
+
+    def load_config(self, filepath=None):
+
+        if filepath is None:
+            filepath = os.path.join("results", "default_model_config.json")
+
+        with open(filepath, "r") as f:
+            config = json.load(f)
+
+        return config
+
+    def restore_config(self, filepath=None):
+
+        if filepath is None:
+            filepath = os.path.join("results", "default_model_config.json")
+
+        config = self.load_config(filepath)
+
+        self._apply_config(config)
 
     def initialize_agents(self):
 
