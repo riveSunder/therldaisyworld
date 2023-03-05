8665c91233c66fe672ab57220015ee22fac09bb8
rivesunder <rivesunder@protonmail.com>
2023-03-05 15:50:32 -0700

    update experimental daisyworld with seasons and inclinations

--- daisy/experimental/daisy_world_rl.py
+++ daisy/experimental/daisy_world_rl.py
@@ -1,11 +1,13 @@
+import os
+
+import json
+
 import numpy as np
+import numpy.random as npr
+import time
 
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-
-import matplotlib.pyplot as plt
-import matplotlib
+from daisy.nn.functional import ft_convolve,\
+        pad_to_2d
 
 class RLDaisyWorld():
 
@@ -14,7 +16,7 @@ class RLDaisyWorld():
         # channels
         self.ch = 5
         # batch_size 
-        self.batch_size = 1
+        self.batch_size = 4
         # size of the toroidal daisyworld
         self.dim = 8
 
@@ -29,14 +31,14 @@ class RLDaisyWorld():
         self.agent_gamma = 0.05
         self.q = 0.2 * self.S / self.sigma
         self.Toptim = 295.5
-        self.dt = 0.01
+        self.dt = 0.1
         self.ddL = 0.
         
         # stellar luminosity R[0.,2.]
-        self.max_L = 1.3
-        self.min_L = 0.65
+        self.max_L = 1.2
+        self.min_L = 0.7
         self.initial_L = self.min_L
-        self.ramp_period = 512 
+        self.ramp_period = 256 
 
         # seasonal parameters
         self.use_seasons = True
@@ -60,53 +62,136 @@ class RLDaisyWorld():
 
         self.n_agents = 3
 
-        self.kernel_radius = 5
         self.initialize_neighborhood()
         self.initialize_agents()
         self.reset()
 
+
+    def make_config(self):
+
+        config = {}
+
+        config["max_L"] = self.max_L
+        config["min_L"] = self.min_L
+        config["initial_L"] = self.initial_L
+        config["ramp_period"] = self.ramp_period
+        config["dL"] = self.dL
+        config["p"] = self.p
+        config["g"] = self.g
+        config["S"] = self.S
+        config["sigma"] = self.sigma
+        config["gamma"] = self.gamma
+        config["albedo_bare"] = self.albedo_bare
+        config["albedo_light"] = self.albedo_light
+        config["albedo_dark"] = self.albedo_dark
+        config["temp_optimal"] = self.temp_optimal
+        config["light_proportion"] = self.light_proportion
+        config["dark_proportion"] = self.dark_proportion
+        config["initial_al"] = self.initial_al
+        config["initial_ad"] = self.initial_ad
+        config["n_agents"] = self.n_agents
+        config["agent_gamma"] = self.agent_gamma
+
+        return config
+
+    def save_config(self, filepath=None):
+
+        if filepath is None:
+            filepath = os.path.join("results", "default_model_config.json")
+
+        config = self.make_config()
+
+        with open(filepath, "w") as f:
+            json.dump(config, f)
+
+    def _apply_config(self, config):
+
+        self.max_L = config["max_L"]
+        self.min_L = config["min_L"]
+        self.initial_L = config["initial_L"]
+        self.ramp_period = config["ramp_period"]
+        self.dL = config["dL"]
+        self.p = config["p"]
+        self.g = config["g"]
+        self.S = config["S"]
+        self.sigma = config["sigma"]
+        self.gamma = config["gamma"]
+        self.albedo_bare = config["albedo_bare"]
+        self.albedo_light = config["albedo_light"]
+        self.albedo_dark = config["albedo_dark"]
+        self.temp_optimal = config["temp_optimal"]
+        self.light_proportion = config["light_proportion"]
+        self.dark_proportion = config["dark_proportion"]
+        self.initial_al = config["initial_al"]
+        self.initial_ad = config["initial_ad"]
+        self.n_agents = config["n_agents"]
+        self.agent_gamma = config["agent_gamma"]
+
+    def load_config(self, filepath=None):
+
+        if filepath is None:
+            filepath = os.path.join("results", "default_model_config.json")
+
+        with open(filepath, "r") as f:
+            config = json.load(f)
+
+        return config
+
+    def restore_config(self, filepath=None):
+
+        if filepath is None:
+            filepath = os.path.join("results", "default_model_config.json")
+
+        config = self.load_config(filepath)
+
+        self._apply_config(config)
+
     def initialize_agents(self):
 
-        self.agent_indices = torch.randint(self.dim, \
+        self.agent_indices = np.random.randint(self.dim, \
                 size=(self.batch_size, self.n_agents, 2))
 
         # the "bellies" of the agents
-        self.agent_states = torch.ones(self.batch_size, self.n_agents, 1)
+        self.agent_states = np.ones((self.batch_size, self.n_agents, 1))
 
     def update_agents(self, action):
 
         # TODO: agents aren't allowed to occupy the same cells in a grid
         # (required for multiagent env mode)
-        self.agent_states = torch.clamp(self.agent_states - self.agent_gamma, 0.,1.)
+        self.agent_states = np.clip(self.agent_states - self.agent_gamma, 0.,1.)
 
         for bb in range(action.shape[0]):
             for nn in range(action.shape[1]):
-                
-                if action[bb,nn,0] == 8:
-                    pass
-                    # no eating or movement
-                elif action[bb,nn,0] % 4 == 0:
-                    self.agent_indices[bb,nn,1] -= 1
-                elif action[bb,nn,0] % 4 == 1:
-                    self.agent_indices[bb,nn,0] -= 1
-                elif action[bb,nn,0] % 4 == 2:
-                    self.agent_indices[bb,nn,1] += 1
-                elif action[bb,nn,0] % 4 == 3:
-                    self.agent_indices[bb,nn,0] += 1
+                # dead agents don't move
+                if self.agent_states[bb,nn] > 0.0:
+                    
+                    if action[bb,nn,0] == 8:
+                        pass
+                        # no eating or movement
+                    elif action[bb,nn,0] % 4 == 0:
+                        self.agent_indices[bb,nn,1] -= 1
+                    elif action[bb,nn,0] % 4 == 1:
+                        self.agent_indices[bb,nn,0] -= 1
+                    elif action[bb,nn,0] % 4 == 2:
+                        self.agent_indices[bb,nn,1] += 1
+                    elif action[bb,nn,0] % 4 == 3:
+                        self.agent_indices[bb,nn,0] += 1
 
-                self.agent_indices[bb,nn,:] = self.agent_indices[bb,nn,:] % self.dim
+                    self.agent_indices = self.agent_indices % self.dim
 
-                if action[bb,nn,0] > 4:
-                    # actions 4 through 8 indication grazing movement
-                    xx, yy = self.agent_indices[bb,nn,0], self.agent_indices[bb,nn,1]
-                    self.agent_states[bb,nn,0] += self.grid[bb,1:3,xx,yy].sum()
-                    self.grid[bb,1:3,xx,yy] *= 0.0
+                    if action[bb,nn,0] > 4:
+                        # actions 4 through 8 indication grazing movement
+                        xx, yy = self.agent_indices[bb,nn,0], self.agent_indices[bb,nn,1]
+                        self.agent_states[bb,nn,0] += self.grid[bb,1:3,xx,yy].sum()
+                        self.grid[bb,1:3,xx,yy] *= 0.0
 
 
     def get_obs(self, agent_indices=None):
 
-        obs = torch.zeros(*agent_indices.shape[:2], 5, 3, 3)
-        obs_grid = F.pad(self.grid, (1,1,1,1), mode="circular") 
+        obs = np.zeros((*agent_indices.shape[:2], self.ch, 3, 3))
+        pad_dims = (*self.grid.shape[:-2], self.grid.shape[-2] + 2, self.grid.shape[-1] + 2)
+        obs_grid = pad_to_2d(self.grid, dims=pad_dims, mode="circular") 
+
 
         for bb in range(obs.shape[0]):
             for nn in range(obs.shape[1]):
@@ -114,75 +199,39 @@ class RLDaisyWorld():
                 y_start = agent_indices[bb,nn,1]
 
                 obs[bb,nn,:,:,:] = obs_grid[\
-                        bb,nn,x_start:x_start+3, y_start:y_start+3]
+                        bb,:,x_start:x_start+3, y_start:y_start+3]
 
         return obs
 
     def initialize_neighborhood(self):
 
         ## Convolution for daisy density
-        # central kernel weight is 0.67, adjacent weights = .37/8
-        eps  = 1e-5
-        x = np.arange(-1,1 + 2/(2*self.kernel_radius) , 2/ (2*self.kernel_radius))
-        xx, yy = np.meshgrid(x, x)
-        rr = xx**2 + yy**2
-
-        gaussian = lambda r: 1.0 #np.exp(-r**2 / 2.) 
-
+        # kernel based on Gaussian
         self.n_daisies = 2
-        self.daisy_kernel = torch.ones(1,1,2*self.kernel_radius+1,2*self.kernel_radius+1) #* gaussian(rr) 
-        self.daisy_kernel /= eps + self.daisy_kernel.sum()
-        #self.daisy_kernel / self.kernel.sum()
+        self.daisy_kernel = np.ones((1,1,3,3)) * np.exp(-1)
+        self.daisy_kernel[:,:,1,1] = 1.0 
+        self.daisy_kernel[:,:,0::2, 0::2] = np.exp(-2) 
+        self.daisy_kernel /= self.daisy_kernel.sum() 
         kernel_dim = self.daisy_kernel.shape[-1]
-        padding = (kernel_dim - 1) // 2
-
-        self.daisy_conv = nn.Conv2d(1, 1,\
-                kernel_dim, padding=padding,\
-                padding_mode="circular", \
-                bias=False)
-
-        for param in self.daisy_conv.named_parameters():
-            param[1].requires_grad = False
-            param[1][:] = self.daisy_kernel
+        padding = 0 #(kernel_dim - 1) // 2
 
         # local and adjacent albedo convs
-        self.local_albedo_kernel = torch.zeros(1, 1, 3,3)
+        self.local_albedo_kernel = np.zeros((1, 1, 3,3))
         self.local_albedo_kernel[:,:,1,1] = 1.0
-        self.adjacent_albedo_kernel = torch.ones(1,1,2*self.kernel_radius+1,2*self.kernel_radius+1) \
-                / ((self.kernel_radius*2+1)**2 - 1)
-
-        self.adjacent_albedo_kernel[:,:,self.kernel_radius+1, self.kernel_radius+1] = 0.
-        #self.adjacent_albedo_kernel /= eps + self.adjacent_albedo_kernel.sum()
+        self.adjacent_albedo_kernel = np.ones((1, 1, 3,3)) / 8. 
+        self.adjacent_albedo_kernel[:,:,0,0] = 0.
         kernel_dim = self.local_albedo_kernel.shape[-1]
-        padding = (kernel_dim - 1) // 2
-
-        self.local_albedo_conv = nn.Conv2d(1, 1,
-                kernel_dim, padding=padding,\
-                padding_mode="circular", \
-                bias=False)
-
-        self.adjacent_albedo_conv = nn.Conv2d(1, 1,
-                self.kernel_radius*2+1, padding=self.kernel_radius,\
-                padding_mode="circular", \
-                bias=False)
-
-        for param in self.local_albedo_conv.named_parameters():
-            param[1].requires_grad = False
-            param[1][:] = self.local_albedo_kernel
-
-        for param in self.adjacent_albedo_conv.named_parameters():
-            param[1].requires_grad = False
-            param[1][:] = self.adjacent_albedo_kernel
+        padding = 0 
 
     def initialize_grid(self):
 
-        dark_probability = torch.rand(\
+        dark_probability = np.random.rand(\
                 self.batch_size,\
                 2,\
                 self.dim,\
                 self.dim)
 
-        light_probability = torch.rand(\
+        light_probability = np.random.rand(\
                 self.batch_size,\
                 2,\
                 self.dim,\
@@ -193,11 +242,11 @@ class RLDaisyWorld():
         light_daisies = 1.0 * (light_probability[:,0,:,:] < self.light_proportion) \
                 * self.initial_al * light_probability[:,1,:,:] 
 
-        grid =  torch.zeros(\
+        grid =  np.zeros((\
                 self.batch_size,\
                 self.ch,\
                 self.dim,\
-                self.dim)
+                self.dim))
 
         grid[:,0,...] = self.p - light_daisies - dark_daisies
         grid[:,1,...] = light_daisies
@@ -210,7 +259,7 @@ class RLDaisyWorld():
         beta = self.calculate_growth_rate(temp)
         growth = self.calculate_growth(beta, daisy_density)
 
-        grid[:,3,:,:] = temp
+        grid[:,3:4,:,:] = temp
         self.grid = grid
 
 
@@ -222,9 +271,11 @@ class RLDaisyWorld():
         self.step_count = 0
         self.update_inclination()
         self.initialize_grid()
-        self.initialize_neighborhood()
+        self.initialize_agents()
 
         obs = self.get_obs(self.agent_indices)
+
+        return obs
 
     def calculate_growth_rate(self, temp):
         beta = 1 - self.g*(self.temp_optimal - temp)**2 
@@ -243,10 +294,10 @@ class RLDaisyWorld():
         # bare ground available for growth 
         a_b = self.p - a_l - a_d 
 
-        dl_dt = a_l*(a_b * beta - self.gamma)
-        dd_dt = a_d*(a_b * beta - self.gamma)
+        dl_dt = a_l*(a_b * beta.squeeze() - self.gamma)
+        dd_dt = a_d*(a_b * beta.squeeze() - self.gamma)
 
-        growth = torch.zeros_like(daisy_density)
+        growth = np.zeros_like((daisy_density))
         growth[:,0,...] = dl_dt 
         growth[:,1,...] = dd_dt
 
@@ -259,14 +310,17 @@ class RLDaisyWorld():
         # groundcover has 3 channels (bare, light daisies, dark daisies)
 
         groundcover[:,0,...] = self.p - groundcover[:,1,:,:] - groundcover[:,2,:,:] 
-        local_albedo = torch.zeros(1,1,self.dim, self.dim) 
-        adjacent_albedo = torch.zeros(1,1,self.dim, self.dim) 
+        local_albedo = np.zeros(\
+                (self.batch_size,1,self.dim, self.dim) )
+        adjacent_albedo = np.zeros(\
+                (self.batch_size,1,self.dim, self.dim))
 
         for ii, albedo in enumerate([\
                 self.albedo_bare, self.albedo_light, self.albedo_dark]):
 
             local_albedo += albedo * groundcover[:,ii:ii+1,:,:]
-            adjacent_albedo += albedo * self.adjacent_albedo_conv(groundcover[:,ii:ii+1,:,:])
+            adjacent_albedo += albedo * ft_convolve(\
+                    groundcover[:,ii:ii+1,:,:], self.adjacent_albedo_kernel)
     
         return local_albedo, adjacent_albedo
 
@@ -275,12 +329,15 @@ class RLDaisyWorld():
         # local albedo 
         Al = local_albedo
         # neighborhood albedo
-        A = adjacent_albedo
+        A = adjacent_albedo #.mean()
         
         # effective radiation temperature
         self.temp_effective = (\
-                (self.S * self.L * self.inclination\
-                * (1-A))/self.sigma)**(1/4)
+                (self.S*self.L * self.inclination * (1-A))/self.sigma)**(1/4)
+
+        dead_effective = (\
+                (self.S * self.L * self.inclination * (1-self.albedo_bare))/self.sigma)**(1/4)
+        self.dead_temp = dead_effective
 
         temp = (self.q*(A - Al) + self.temp_effective**4)**(1/4)
         self.temp = temp
@@ -289,10 +346,12 @@ class RLDaisyWorld():
 
     def calculate_daisy_density(self, local_daisies):
 
-        daisy_density = torch.zeros(1,2, self.dim, self.dim)
+        daisy_density = np.zeros((self.batch_size,2, self.dim, self.dim))
 
         for jj in range(self.n_daisies):
-            daisy_density[:,jj:jj+1,:,:] = self.daisy_conv(local_daisies[:,jj:jj+1,:,:])
+            daisy_density[:,jj:jj+1,:,:] = ft_convolve(local_daisies[:,jj:jj+1,:,:], \
+                    self.daisy_kernel)
+
 
         return daisy_density
 
@@ -306,15 +365,16 @@ class RLDaisyWorld():
         growth = self.calculate_growth(beta, daisy_density)
 
         new_grid = 0. * grid
-        grid[:,3,:,:] = temp
-        new_grid[:,1:3, :,:] = torch.clamp(grid[:,1:3, :,:] + self.dt * growth, 0,1)
+        grid[:,3:4,:,:] = temp
+        new_grid[:,1:3, :,:] = np.clip(grid[:,1:3, :,:] + self.dt * growth, 0,1)
         new_grid[:,0, :,:] = self.p - new_grid[:,1, :,:] - new_grid[:,2,:,:] #.sum(dim=1)
 
-        new_grid = torch.round(new_grid, decimals=3)
+        new_grid = np.round(new_grid, decimals=3)
 
         if self.n_agents:
             for bb in range(self.batch_size):
                 for nn in range(self.n_agents):
+                    
                     xx, yy = self.agent_indices[bb,nn,0], self.agent_indices[bb,nn,1]
                     new_grid[bb,4,xx,yy] = self.agent_states[bb,nn]
 
@@ -344,41 +404,42 @@ class RLDaisyWorld():
 
             yy, xx = np.meshgrid(inclination_x, inclination_x)
 
-            xx =  torch.cos(torch.tensor(xx)) #tensor(xx, requires_grad=False)**2
-            self.inclination = 1.0 + self.amplitude_seasonal * xx #(1.0 - xx).unsqueeze(0).unsqueeze(0)
+            xx =  np.cos(xx)
+            self.inclination = 1.0 + self.amplitude_seasonal * xx 
 
             # inclination modifier is adjusted to have mean luminosity 1.0
             #self.inclination /= self.inclination.mean()
         else:
 
-            self.inclination = torch.ones(1,1,self.dim, self.dim)
+            self.inclination = np.ones((1,1,self.dim, self.dim))
 
     def step(self, action=None):
         
         if action is None and self.n_agents:
-            action = torch.zeros(self.batch_size, self.n_agents, 1)
+            action = np.zeros((self.batch_size, self.n_agents,1))
 
         if action is not None:
-            my_action = torch.zeros(self.batch_size, self.n_agents, 1)
-            my_action[:,:action.shape[1],:] = action
-
-            self.update_agents(my_action)
+            self.update_agents(action)
 
         for ii in range(int(1. / self.dt)):
             self.grid = self.forward(self.grid) 
 
         obs = self.get_obs(self.agent_indices)
-        reward = 1.0 * self.agent_states.detach()
+        if self.n_agents:
+            reward = 1.0 * self.agent_states
+        else:
+            reward = self.grid[:,1:3,:,:].sum(axis=(-2,-1)) > 0
+
+        reward = reward * (reward > 0)
         done = reward < 0.1
-        global_done = obs[:,:,1:3,:,:].max(dim=2, keepdim=True)[0].max(dim=3, keepdim=True)[0].max(dim=4)[0]
 
-        done, info = 0, {}
-
+        info = {}
         self.L = self.update_L(self.L)
+
         if self.use_seasons:
             self.update_inclination()
 
-        return reward, obs, done, info
+        return obs, reward, done, info
 
     def __call__(self, grid): 
 
@@ -395,10 +456,12 @@ if __name__ == "__main__":
 
     for ii in range(9):
         for jj in range(1):
-            action = torch.tensor([[[ii]]]) #randint(9, size=(env.batch_size, env.n_agents, 1))
+            action = np.array([[[ii]]]) #randint(9, size=(env.batch_size, env.n_agents, 1))
             
+            print(env.grid[:,4,:,:])
 
             env.step(action)
 
 
+    print(a.shape, b.shape)
 
