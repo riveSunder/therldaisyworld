9c22e3a1b9d9e6c5de0600a6dacfc9fa148135e4
Your Name <you@example.com>
2023-05-10 12:39:49 -0600

    add tests for evo algos

--- daisy/tests/test_all.py
+++ daisy/tests/test_all.py
@@ -1,6 +1,8 @@
 import unittest
 
 from daisy.tests.daisy.test_daisy_world_rl import TestRLDaisyWorld
+from daisy.tests.daisy.evo.test_cmaes import TestCMAES
+from daisy.tests.daisy.evo.test_sges import TestSimpleGaussianES
 
 
 if __name__ == "__main__":
