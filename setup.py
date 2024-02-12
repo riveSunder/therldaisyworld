943598b42c2f1f125561c33704049f811fba5c78
RiveSunder <rivesunder@protonmail.com>
2024-02-12 10:35:19 -0700

    add mpi4py to setup.py depends

--- setup.py
+++ setup.py
@@ -5,7 +5,8 @@ setup(name="daisy",\
         version = "0.0",\
         description = "The RL DaisyWorld", \
         install_requires = ["numpy==1.24.2",\
-                "matplotlib==3.7.0"] \
+                "matplotlib==3.7.0",\
+                "mpi4py==3.1.5"] \
         )
 
     
